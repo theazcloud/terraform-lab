55f0e53f742fdbb488a5f779a7594aeccd0bcbd6
Paul Trotman <paul.trotman@zebrait.com>
2022-01-16 11:39:37 +0000

    Update main.tf

--- Templates/main.tf
+++ Templates/main.tf
@@ -10,10 +10,10 @@ terraform {
 provider "azurerm" {
     features {}
 
-    client_id         = $env.ARM_CLIENT_ID
-    client_secret     = $env.ARM_CLIENT_SECRET
-    subscription_id   = $env.ARM_SUBSCRIPTION_ID
-    tenant_id         = $env.ARM_TENANT_ID
+    client_id         = $env:ARM_CLIENT_ID
+    client_secret     = $env:ARM_CLIENT_SECRET
+    subscription_id   = $env:ARM_SUBSCRIPTION_ID
+    tenant_id         = $env:ARM_TENANT_ID
 }
 
 
