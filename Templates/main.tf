dd79cd8d096593f44488d0d47f6527a408e2f651
Paul Trotman <paul.trotman@zebrait.com>
2022-01-16 19:17:56 +0000

    updated

--- Templates/main.tf
+++ Templates/main.tf
@@ -1,29 +1,33 @@
 terraform {
-    required_providers {
-        azurerm = {
-        source = "hashicorp/azurerm"
-        version = "~>2.0"
-        }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>2.0"
     }
+  }
 }
 
 provider "azurerm" {
-    features {}
+  features {}
 
-    client_id         = ""
-    client_secret     = ""
-    subscription_id   = ""
-    tenant_id         = ""
+  client_id       = ""
+  client_secret   = ""
+  subscription_id = ""
+  tenant_id       = ""
 }
 
 
 # Your code goes here
 
-resource "random_pet" "rg-name" {
-    prefix    = var.resource_group_name_prefix
+resource "azurerm_resource_group" "resource_groups_hub" {
+
+  name     = "hub-core-sec-rsg"
+  location = var.resource_group_location
 }
 
-resource "azurerm_resource_group" "rg" {
-    name      = random_pet.rg-name.id
-    location  = var.resource_group_location
+resource "azurerm_resource_group" "resource_groups_sec" {
+
+  name     = "hub-operations-rsg"
+  location = var.resource_group_location
+
 }
