87671c9e660c7b6361d9db72e90b86ce4741f452
Paul Trotman <paul.trotman@zebrait.com>
2022-01-23 19:53:52 +0000

    updated files

--- Templates/terraform_init.sh
+++ Templates/terraform_init.sh
@@ -3,8 +3,6 @@ terraform init \
     -backend-config="client_secret=$ARM_CLIENT_SECRET" \
     -backend-config="subscription_id=$ARM_SUBSCRIPTION_ID" \
     -backend-config="tenant_id= $ARM_TENANT_ID" \
-
-
-export ARM_BILLING_ACCOUNT_NAME  $ARM_BILLING_ACCOUNT_NAME
-export ARM_BILLING_PROFILE_NAME  $ARM_BILLING_PROFILE_NAME
-export ARM_INVOICE_SECTION_NAME  $ARM_INVOICE_SECTION_NAME
+    -backend-config="billing_account_name= $ARM_BILLING_ACCOUNT_NAME" \
+    -backend-config="billing_profile_name= $ARM_BILLING_PROFILE_NAME" \
+    -backend-config="invoice_section_name= $ARM_INVOICE_SECTION_NAME" \
