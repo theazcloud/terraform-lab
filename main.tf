6b5ab3dc26590190897d1e59b96ac993b9a1ea22
Paul Trotman <paul.trotman@zebrait.com>
2022-01-28 12:29:09 +0000

    updated main.tf

--- main.tf
+++ main.tf
@@ -15,3 +15,41 @@ provider "azurerm" {
   subscription_id = ""
   tenant_id       = ""
 }
+
+# Get the current client configuration from the AzureRM provider.
+# This is used to populate the root_parent_id variable with the
+# current Tenant ID used as the ID for the "Tenant Root Group"
+# Management Group.
+
+data "azurerm_client_config" "core" {}
+
+# Use variables to customize the deployment
+
+variable "root_id" {
+  type    = string
+  default = "es"
+}
+
+variable "root_name" {
+  type    = string
+  default = "Enterprise-Scale"
+}
+
+# Declare the Terraform Module for Cloud Adoption Framework
+# Enterprise-scale and provide a base configuration.
+
+module "enterprise_scale" {
+  source  = "Azure/caf-enterprise-scale/azurerm"
+  version = ">= 1.0.0"
+
+  providers = {
+    azurerm              = azurerm
+    azurerm.connectivity = azurerm
+    azurerm.management   = azurerm
+  }
+
+  root_parent_id = data.azurerm_client_config.core.tenant_id
+  root_id        = var.root_id
+  root_name      = var.root_name
+
+}
