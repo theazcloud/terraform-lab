76d3c8385d02654de2b81bb1714d9b77b81ed0af
Paul Trotman <paul.trotman@zebrait.com>
2022-02-02 21:26:23 +0000

    updated

--- 01_Terraform_Initial_Setup.ps1
+++ 01_Terraform_Initial_Setup.ps1
@@ -16,7 +16,9 @@ $GithubPath = 'D:\Github'
 
 Import-CliXml -path $cliXMLPath | ForEach-Object { Set-Item "env:$($_.Name)" $_.Value }
 
-Remove-Item -path $cliXMLPath -Force
+gci env:ARM_*
+
+#Remove-Item -path $cliXMLPath -Force
 
 
 [hashtable]$SubPreReq = Import-CliXml -path $subInfoPath
@@ -27,7 +29,7 @@ $BILLING_PROFILE_NAME = $SubPreReq.BILLING_PROFILE_NAME
 
 $INVOICE_SECTION_NAME = $SubPreReq.INVOICE_SECTION_NAME
 
-Remove-Item -path $subInfoPath -Force
+#Remove-Item -path $subInfoPath -Force
 
 Set-Location -path $GithubPath\terraform-lab
 
