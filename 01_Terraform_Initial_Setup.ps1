73923eeb370da470547fb85a6784b4ec29fb27fe
Paul Trotman <paul.trotman@zebrait.com>
2022-02-08 21:20:35 +0000

    updated files

--- 01_Terraform_Initial_Setup.ps1
+++ 01_Terraform_Initial_Setup.ps1
@@ -37,4 +37,8 @@ terraform init
 
 terraform plan -var "billing_account_name=${BILLING_ACCOUNT_NAME}" -var "billing_profile_name=${BILLING_PROFILE_NAME}" -var "invoice_section_name=${INVOICE_SECTION_NAME}" -out main.tfplan
 
-#terraform apply "main.tfplan"
+terraform apply "main.tfplan"
+
+terraform plan -var "billing_account_name=${BILLING_ACCOUNT_NAME}" -var "billing_profile_name=${BILLING_PROFILE_NAME}" -var "invoice_section_name=${INVOICE_SECTION_NAME}" -destroy -out destroy.main.tfplan
+
+terraform apply "destroy.main.tfplan"
