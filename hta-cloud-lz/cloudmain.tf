9958200b5df9580f322ceb3486b93f9b31d52f92
Paul Trotman <paul.trotman@zebrait.com>
2022-02-19 17:49:48 +0000

    Update cloudmain.tf

--- hta-cloud-lz/cloudmain.tf
+++ hta-cloud-lz/cloudmain.tf
@@ -14,9 +14,16 @@ terraform {
     }
 }
 
+variable "ARM_CLIENT_ID" {}
+variable "ARM_CLIENT_SECRET" {}
+variable "ARM_SUBSCRIPTION_ID" {}
+variable "ARM_TENANT_ID" {}
 provider "azurerm" {
   features {}
-
+      client_id       = "${var.ARM_CLIENT_ID}"
+      client_secret   = "${var.ARM_CLIENT_SECRET}"
+      subscription_id = "${var.ARM_SUBSCRIPTION_ID}"
+      tenant_id       = "${var.ARM_TENANT_ID}"
 }
 
 data "azurerm_client_config" "core" {}
