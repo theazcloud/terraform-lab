c3f52a66fabeaf52de26ef508319301baa193cb9
Paul Trotman <paul.trotman@zebrait.com>
2022-02-19 17:38:29 +0000

    Update cloudmain.tf

--- hta-cloud-lz/cloudmain.tf
+++ hta-cloud-lz/cloudmain.tf
@@ -22,3 +22,14 @@ provider "azurerm" {
 data "azurerm_client_config" "core" {}
 
 variable  "ARM_BILLING_ACCOUNT_NAME" {}
+
+variable "ARM_BILLING_PROFILE_NAME" {}
+
+variable "ARM_INVOICE_SECTION_NAME" {}
+
+
+data "azurerm_billing_mca_account_scope" "demo" {
+  billing_account_name =  "${var.ARM_BILLING_ACCOUNT_NAME}"
+  billing_profile_name =  "${var.ARM_BILLING_PROFILE_NAME}"
+  invoice_section_name =  "${var.ARM_INVOICE_SECTION_NAME}"
+}
