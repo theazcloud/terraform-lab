fdf17eab2b60e53c9e27d20a32290de3c6ff7205
Paul Trotman <paul.trotman@zebrait.com>
2022-02-19 17:16:40 +0000

    Update cloudmain.tf

--- hta-cloud-lz/cloudmain.tf
+++ hta-cloud-lz/cloudmain.tf
@@ -9,7 +9,7 @@ terraform {
     required_providers {
         azurerm = {
         source  = "hashicorp/azurerm"
-        version = "~>2.0"
+        version = ">=2.46.0"
         }
     }
 }
@@ -17,10 +17,6 @@ terraform {
 provider "azurerm" {
   features {}
 
-  client_id       = TFVAR_client_id
-  client_secret   = TFVAR_client_secret
-  subscription_id = TFVAR_subscription_id
-  tenant_id       = TFVAR_tenant_id
 }
 
 data "azurerm_client_config" "core" {}
